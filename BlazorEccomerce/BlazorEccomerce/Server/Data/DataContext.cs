598b568ac73233611db86367b2f9e997991b3090
Lene7 <lenekriel84.lk@gmail.com>
2024-07-10 15:19:49 +0200

    Product Variants
    Add Product variants and Types. Seeding, migrations and update database.

--- BlazorEccomerce/BlazorEccomerce/Server/Data/DataContext.cs
+++ BlazorEccomerce/BlazorEccomerce/Server/Data/DataContext.cs
@@ -9,6 +9,17 @@
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+			modelBuilder.Entity<ProductVariant>()
+				.HasKey(p => new { p.ProductId, p.ProductTypeId });
+
+			modelBuilder.Entity<ProductType>().HasData(
+					new ProductType { Id = 1, Name = "Sunrise"},
+					new ProductType { Id = 2, Name = "Midday" },
+					new ProductType { Id = 3, Name = "Sunset" },
+					new ProductType { Id = 4, Name = "Whole morning" },
+					new ProductType { Id = 5, Name = "Whole afternoon" }
+				);
+
 			modelBuilder.Entity<Category>().HasData(
 				new Category
 				{
@@ -30,7 +41,7 @@
 					Name = "Live Photoshoots",
 					Url = "live-photoshoots"
 				}
-				);
+			);
 
 			modelBuilder.Entity<Product>().HasData(
 					new Product
@@ -39,7 +50,6 @@
 						Title = "Personal Photoshoot",
 						Description = "One (1) hour photoshoot. This include between 80 to a 100 edited photos. This can be for portfolio, formals, social media, or just for the fun of it!",
 						ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjUgaoL9tabSf-Ig21XIdTawnh-BgUEbQZWjFisvLr9hf3-ekVFntcXFCAHAlKoV24M6Q",
-						Price = 1600.00m,
 						CategoryId = 1
 					},
 
@@ -49,7 +59,6 @@
 						Title = "Couples Photoshoot",
 						Description = "Two (2) hours photoshoot. This include between 100 to 150 edited photos. This can be for special occasions, social media, or just for the love you share!",
 						ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNk9UUKvmqxEh2t17_5jCYBWO4e4mWK2sfw&s",
-						Price = 2000.00m,
 						CategoryId = 2
 					},
 
@@ -59,13 +68,73 @@
 						Title = "Live Photography",
 						Description = "Three (3) hours photoshoot. This include between 200 to 300 edited photos. This can be for events, live performances, or social media!",
 						ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4JQhSxjiWp9Q3ClqBHpGwnoQI_MvLlmDph4xpBJq3N-I9zmsb1e-DJgPfEyupgMWgr9U",
-						Price = 5000.00m,
 						CategoryId = 3
+					}
+			);
+
+			modelBuilder.Entity<ProductVariant>().HasData(
+					new ProductVariant
+					{
+						ProductId = 1,
+						ProductTypeId = 1,
+						Price = 1400.00m,
+						OriginalPrice = 1600.00m
+					},
+					new ProductVariant
+					{
+						ProductId = 1,
+						ProductTypeId = 2,
+						Price = 1500.00m,
+						OriginalPrice = 1600.00m
+					},
+					new ProductVariant
+					{
+						ProductId = 1,
+						ProductTypeId = 3,
+						Price = 1600.00m,
+						OriginalPrice = 1600.00m
+					},
+					new ProductVariant
+					{
+						ProductId = 2,
+						ProductTypeId = 1,
+						Price = 1800.00m,
+						OriginalPrice = 2000.00m
+					},
+					new ProductVariant
+					{
+						ProductId = 2,
+						ProductTypeId = 2,
+						Price = 1900.00m,
+						OriginalPrice = 2000.00m
+					},
+					new ProductVariant
+					{
+						ProductId = 2,
+						ProductTypeId = 3,
+						Price = 2000.00m,
+						OriginalPrice = 2000.00m
+					},
+					new ProductVariant
+					{
+						ProductId = 3,
+						ProductTypeId = 4,
+						Price = 4500.00m,
+						OriginalPrice = 5000.00m
+					},
+					new ProductVariant
+					{
+						ProductId = 3,
+						ProductTypeId = 5,
+						Price = 5000.00m,
+						OriginalPrice = 5000.00m
 					}
 				);
 		}
 
 		public DbSet<Product> Products { get; set; }
 		public DbSet<Category> Categories { get; set; }
+		public DbSet<ProductType> ProductTypes { get; set; }
+		public DbSet<ProductVariant> ProductVariants { get; set; }
 	}
 }
---
+++ BlazorEccomerce/BlazorEccomerce/Server/Migrations/20240710131433_ProductVariant.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEccomerce.Server.Migrations
+{
+    public partial class ProductVariant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "ProductTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductVariants",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductTypeId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OriginalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductVariants", x => new { x.ProductId, x.ProductTypeId });
+                    table.ForeignKey(
+                        name: "FK_ProductVariants_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductVariants_ProductTypes_ProductTypeId",
+                        column: x => x.ProductTypeId,
+                        principalTable: "ProductTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Sunrise" },
+                    { 2, "Midday" },
+                    { 3, "Sunset" },
+                    { 4, "Whole morning" },
+                    { 5, "Whole afternoon" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductVariants",
+                columns: new[] { "ProductId", "ProductTypeId", "OriginalPrice", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, 1600.00m, 1400.00m },
+                    { 1, 2, 1600.00m, 1500.00m },
+                    { 1, 3, 1600.00m, 1600.00m },
+                    { 2, 1, 2000.00m, 1800.00m },
+                    { 2, 2, 2000.00m, 1900.00m },
+                    { 2, 3, 2000.00m, 2000.00m },
+                    { 3, 4, 5000.00m, 4500.00m },
+                    { 3, 5, 5000.00m, 5000.00m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductVariants_ProductTypeId",
+                table: "ProductVariants",
+                column: "ProductTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductVariants");
+
+            migrationBuilder.DropTable(
+                name: "ProductTypes");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 1600.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 2000.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 5000.00m);
+        }
+    }
+}
