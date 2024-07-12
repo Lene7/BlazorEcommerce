eb36049edf44bd8f99c339bde725a92d49412963
Lene7 <lenekriel84.lk@gmail.com>
2024-07-12 15:43:13 +0200

    search pagination
    If you search a product only two will show up per page

--- BlazorEccomerce/BlazorEccomerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEccomerce/BlazorEccomerce/Client/Services/ProductService/IProductService.cs
@@ -6,10 +6,13 @@
 
 		List<Product> Products { get; set; }
 		string Messsage { get; set; }
+		int CurrentPage { get; set; }
+		int PageCount { get; set; }
+		string LastSearchText { get; set; }
 
 		Task GetProducts(string? categoryUrl = null);
 		Task<ServiceResponse<Product>> GetProduct(int productId);
-		Task SearchProducts(string searchText);
+		Task SearchProducts(string searchText, int page);
 		Task<List<string>> GetProductSearchSuggestions(string searchText);
 	}
 }
