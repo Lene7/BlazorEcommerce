74c41b1923d6584afe6ff36f89c1813a3d0a3730
Lene7 <lenekriel84.lk@gmail.com>
2024-07-22 14:42:39 +0200

    Cart
    Add Cart button and page. As well as functionality to add item to cart as well as delete item from cart and the relevant css.

--- BlazorEccomerce/BlazorEccomerce/Client/Program.cs
+++ BlazorEccomerce/BlazorEccomerce/Client/Program.cs
@@ -2,16 +2,20 @@ global using BlazorEccomerce.Shared;
 global using System.Net.Http.Json;
 global using BlazorEccomerce.Client.Services.ProductService;
 global using BlazorEccomerce.Client.Services.CategoryService;
+global using BlazorEccomerce.Client.Services.CartService;
 using BlazorEccomerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorEccomerce/BlazorEccomerce/Client/Services/CartService/ICartService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorEccomerce.Client.Services.CartService
+{
+	public interface ICartService
+	{
+		event Action OnChange;
+		Task AddToCart(CartItem cartItem);
+		Task<List<CartItem>> GetCartItems();
+		Task<List<CartProductResponseDTO>> GetCartProducts();
+		Task RemoveProductFromCart(int productId, int productTypeId);
+	}
+}
